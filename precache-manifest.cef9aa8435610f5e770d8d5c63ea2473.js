7ccfa93fb3b8915bde91b7d38a7152e95169e322
deepak001ahirwar <deepak001ahirwar@gmail.com>
2020-05-22 00:33:07 +0530

    Updates

---
+++ precache-manifest.cef9aa8435610f5e770d8d5c63ea2473.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9598d7eed38c72845b3df851ed9d34d5",
+    "url": "//react-deploy/index.html"
+  },
+  {
+    "revision": "737bf132b00d8c20f4bf",
+    "url": "//react-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "169db92b2963a491015e",
+    "url": "//react-deploy/static/js/2.904c5842.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "//react-deploy/static/js/2.904c5842.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "737bf132b00d8c20f4bf",
+    "url": "//react-deploy/static/js/main.42f8c4b7.chunk.js"
+  },
+  {
+    "revision": "81d32128e5cc3fc0f25e",
+    "url": "//react-deploy/static/js/runtime-main.879f56de.js"
+  }
+]);
